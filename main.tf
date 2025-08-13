aac2eab11337bfe7bc10b317a1a80db0b0876da5
Admin <santoshbrdr928@gmail.com>
2025-08-13 20:31:51 +0530

    1 rg added

--- main.tf
+++ main.tf
@@ -6,4 +6,7 @@ resource "azurerm_resource_group" "rg_block16" {
   name     = "santoshpipelinerg2"
   location = "West Europe"
 }
-
+resource "azurerm_resource_group" "rg_block17" {
+  name     = "santoshpipelinerg3"
+  location = "West Europe"
+}
